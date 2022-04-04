73886dd17b4bdb0249f47ccf4324e6a42b2c2b6a
hucheng91 <hucheng9110@gmail.com>
2022-04-04 20:19:17 +0800

    feat: statements

--- src/tokenizer.mjs
+++ src/tokenizer.mjs
@@ -1,4 +1,23 @@
 
+const Spec = [
+  // Whitespace
+  [/^\s+/, null],
+
+  // Comments
+  [/^\/\/.*/, null],              // single line comment "//..."
+  [/^\/\*[\s\S]*?\*\//, null],    // multi line comment "/*...*/"
+
+  // Symbol, delimiters
+  [/^;/,null],
+
+  // Number
+  // [/\d+/,'NUMBER'],
+  [/^\d[\d.e_-]*/, 'NUMBER'],
+
+  // String
+  [/^"[^"]*"/,'STRING'],
+  [/^'[^']*"/,'STRING'],
+]
 export default class Tokenizer {
 
   _string = '';
@@ -10,22 +29,39 @@ export default class Tokenizer {
   }
 
   getNextToken() {
-   if (!this.hasMoreTokens()) {
-     return null;
-   }
+    if (!this.hasMoreTokens()) {
+      return null;
+    }
     const str = this._string.slice(this._cursor);
 
-    if (!Number.isNaN(Number(str[0]))) {
-      let number = '';
-      while (!Number.isNaN(Number(str[this._cursor]))) {
-        number += str[this._cursor++]
+    for (const [regexp, tokenType] of Spec) {
+      let tokenValue = this._match(regexp, str);
+      if (tokenValue === null) {
+        continue;
       }
+
+      if (tokenType === null) {
+        return this.getNextToken();
+      }
+
       return {
-        type: 'NUMBER',
-        value: number
-      };
+        type: tokenType,
+        value: tokenValue
+      }
     }
-    return null;
+    let char = str[0];
+    throw new SyntaxError(`Unexpected token: '${char}'`);
+  }
+
+  _match(regexp, string) {
+    const matched = regexp.exec(string);
+    if (matched === null) {
+      return null;
+    }
+
+    this._cursor += matched[0].length;
+
+    return matched[0];
   }
 
   hasMoreTokens() {
--- test/parser.test.mjs
+++ test/parser.test.mjs
@@ -1,5 +1,6 @@
 import { strict as assert } from 'assert';
 import Parser from '../src/parser.mjs';
+import { testSingleStringLiteral, testSingleNumberLiteral, testMulLiteral, testComments } from './parser/literal.test.mjs'
 
 function testParser() {
   const parser = new Parser();
@@ -9,6 +10,10 @@ function testParser() {
 
 function test() {
   testParser();
+  testSingleStringLiteral();
+  testSingleNumberLiteral();
+  testMulLiteral();
+  testComments();
 }
 
 test();
