1969f8761e93a491b2cc9d8f633dfec5d236481d
hucheng91 <hucheng9110@gmail.com>
2022-04-05 11:52:53 +0800

    test: 添加单测

--- test/parser/blockStatement.test.mjs
+++ test/parser/blockStatement.test.mjs
@@ -3,7 +3,7 @@ import Parser from '../../src/parser.mjs';
 
 
 describe('blockStatement', function () {
-  it('testSingleStringLiteral', function () {
+  it('testSingleBlockStatement', function () {
     const parser = new Parser();
     const ast = parser.parse(`
     {"hello"}
@@ -26,4 +26,188 @@ describe('blockStatement', function () {
       ]
     });
   });
+
+  it('testSingleBlockStatement,number,string', function () {
+    const parser = new Parser();
+    const ast = parser.parse(`
+    {
+      "hello";
+      123
+    }
+    `);
+    assert.deepEqual(ast, {
+      "type": "Program",
+      "body": [
+        {
+          "type": "BlockStatement",
+          "body": [
+            {
+              "type": "ExpressionStatement",
+              "expression": {
+                "type": "StringLiteral",
+                "value": "hello"
+              }
+            },
+            {
+              "type": "ExpressionStatement",
+              "expression": {
+                "type": "NumericLiteral",
+                "value": 123
+              }
+            }
+          ]
+        }
+      ]
+    });
+  });
+
+  it('testSingleLiteral,number,string', function () {
+    const parser = new Parser();
+    const ast = parser.parse(`
+    {
+    }
+    `);
+    assert.deepEqual(ast, {
+      "type": "Program",
+      "body": [
+        {
+          "type": "BlockStatement",
+          "body": []
+        }
+      ]
+    });
+  });
+
+  it('BlockStatement', function () {
+    const parser = new Parser();
+    const ast = parser.parse(`
+    {
+      "hello";
+      123
+    }
+    123;
+    {
+      "HC"
+    }
+    `);
+    assert.deepEqual(ast, {
+      "type": "Program",
+      "body": [
+        {
+          "type": "BlockStatement",
+          "body": [
+            {
+              "type": "ExpressionStatement",
+              "expression": {
+                "type": "StringLiteral",
+                "value": "hello"
+              }
+            },
+            {
+              "type": "ExpressionStatement",
+              "expression": {
+                "type": "NumericLiteral",
+                "value": 123
+              }
+            }
+          ]
+        },
+        {
+          "type": "ExpressionStatement",
+          "expression": {
+            "type": "NumericLiteral",
+            "value": 123
+          }
+        },
+        {
+          "type": "BlockStatement",
+          "body": [
+            {
+              "type": "ExpressionStatement",
+              "expression": {
+                "type": "StringLiteral",
+                "value": "HC"
+              }
+            }
+          ]
+        },
+      ]
+    });
+  });
+
+  it('nestedBlockStatement', function () {
+    const parser = new Parser();
+    const ast = parser.parse(`
+    {
+      "fool";
+      {
+        "hello";
+        123
+      }
+      123;
+      {
+        "HC"
+      }
+    }
+   
+    `);
+    assert.deepEqual(ast, {
+      "type": "Program",
+      "body": [
+        {
+          "type": "BlockStatement",
+          "body": [
+            {
+              "type": "ExpressionStatement",
+              "expression": {
+                "type": "StringLiteral",
+                "value": "fool"
+              }
+            },
+            {
+              "type": "BlockStatement",
+              "body": [
+                {
+                  "type": "ExpressionStatement",
+                  "expression": {
+                    "type": "StringLiteral",
+                    "value": "hello"
+                  }
+                },
+                {
+                  "type": "ExpressionStatement",
+                  "expression": {
+                    "type": "NumericLiteral",
+                    "value": 123
+                  }
+                }
+              ]
+            },
+            {
+              "type": "ExpressionStatement",
+              "expression": {
+                "type": "NumericLiteral",
+                "value": 123
+              }
+            },
+            {
+              "type": "BlockStatement",
+              "body": [
+                {
+                  "type": "ExpressionStatement",
+                  "expression": {
+                    "type": "StringLiteral",
+                    "value": "HC"
+                  }
+                }
+              ]
+            },
+          ]
+        }
+
+      ]
+    });
+  });
+
+
 });
