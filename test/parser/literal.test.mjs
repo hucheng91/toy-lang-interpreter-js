badf5e72c1312f74d82620ed70438b60b4892f5b
hucheng91 <hucheng9110@gmail.com>
2022-04-05 10:26:33 +0800

    feat: add mocha

--- test/parser/literal.test.mjs
+++ test/parser/literal.test.mjs
@@ -1,82 +1,84 @@
 import { strict as assert } from 'assert';
 import Parser from '../../src/parser.mjs';
 
-export function testSingleStringLiteral() {
-  const parser = new Parser();
-  const ast = parser.parse(`"hello"`);
-  console.log("ast:", ast);
-  assert.deepEqual(ast, {
-    type: 'Program',
-    body: [
-      {
-        type: 'ExpressionStatement',
-        expression: { type: 'StringLiteral', value: 'hello' }
-      }
-    ]
+
+describe('literal test', function () {
+  describe('statement', function () {
+    it('testSingleStringLiteral', function () {
+      const parser = new Parser();
+      const ast = parser.parse(`"hello"`);
+      assert.deepEqual(ast, {
+        type: 'Program',
+        body: [
+          {
+            type: 'ExpressionStatement',
+            expression: { type: 'StringLiteral', value: 'hello' }
+          }
+        ]
+      });
+    });
+
+    it('testSingleNumberLiteral', function () {
+      const parser = new Parser();
+      const ast = parser.parse(`122`);
+    
+      assert.deepEqual(ast, {
+        type: 'Program',
+        body: [
+          {
+            type: 'ExpressionStatement',
+            expression: { type: 'NumericLiteral', value: 122 }
+          }
+        ]
+      });
+    });
+
+    it('testMulLiteral', function () {
+      const parser = new Parser();
+      const ast = parser.parse(`
+      "hello"
+      123
+      `);
+    
+      assert.deepEqual(ast, {
+        type: 'Program',
+        body: [
+          {
+            type: 'ExpressionStatement',
+            expression: { type: 'StringLiteral', value: 'hello' }
+          },
+          {
+            type: 'ExpressionStatement',
+            expression: { type: 'NumericLiteral', value: 123 }
+          }
+        ]
+      });
+    });
+
+    it('testComments', function () {
+      const parser = new Parser();
+      const ast = parser.parse(`
+      /**
+       * hello 
+       *
+      **/ 
+      "hello";
+      // number
+      123;
+      `);
+      assert.deepEqual(ast, {
+        type: 'Program',
+        body: [
+          {
+            type: 'ExpressionStatement',
+            expression: { type: 'StringLiteral', value: 'hello' }
+          },
+          {
+            type: 'ExpressionStatement',
+            expression: { type: 'NumericLiteral', value: 123 }
+          }
+        ]
+      });
+    });
   });
-}
-
-export function testSingleNumberLiteral() {
-  const parser = new Parser();
-  const ast = parser.parse(`122`);
-  console.log("ast:", ast);
-
-  assert.deepEqual(ast, {
-    type: 'Program',
-    body: [
-      {
-        type: 'ExpressionStatement',
-        expression: { type: 'NumberLiteral', value: 122 }
-      }
-    ]
-  });
-}
-
-export function testMulLiteral() {
-  const parser = new Parser();
-  const ast = parser.parse(`
-  "hello"
-  123
-  `);
-  console.log("ast:", ast);
-
-  assert.deepEqual(ast, {
-    type: 'Program',
-    body: [
-      {
-        type: 'ExpressionStatement',
-        expression: { type: 'StringLiteral', value: 'hello' }
-      },
-      {
-        type: 'ExpressionStatement',
-        expression: { type: 'NumericLiteral', value: 123 }
-      }
-    ]
-  });
-}
-
-function testComments() {
-  const parser = new Parser();
-  const ast = parser.parse(`
-  /**
-   * hello 
-   *
-  **/ 
-  "hello";
-  // number
-  123;
-  `);
-  assert.deepEqual(ast, {
-    type: 'Program',
-    body: [
-      {
-        type: 'ExpressionStatement',
-        expression: { type: 'StringLiteral', value: 'hello' }
-      },
-      {
-        type: 'ExpressionStatement',
-        expression: { type: 'NumericLiteral', value: 123 }
-      }
-    ]
-  });
-}
+});
